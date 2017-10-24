b446b655f6adfd9d4681780489ff3b106cf5e34f
Mike Skelton <skeltonmr43@gmail.com>
2017-10-23 22:26:59 -0400

    problem 3 euler

--- app.js
+++ app.js
@@ -9,19 +9,30 @@
 // }
 // console.log(sum)
 
+
+
 // Problem 2
 
-let sequence = 0;
-const numbers = [];
-for(let i = 1; i <= 4000000; i += sequence){
-    sequence = i - sequence;
-    numbers.push(i);
-}
+// let sequence = 0;
+// const numbers = [];
+// for(let i = 1; i <= 4000000; i += sequence){
+//     sequence = i - sequence;
+//     numbers.push(i);
+// }
+//
+// let sum = 0;
+// for(let i = 0; i < numbers.length; i++){
+//   if(numbers[i] % 2 == 0){
+//     sum = sum + numbers[i];
+//   }
+// }
+// console.log(sum);
 
-let sum = 0;
-for(let i = 0; i < numbers.length; i++){
-  if(numbers[i] % 2 == 0){
-    sum = sum + numbers[i];
+
+// Problem 3
+
+for(let i = 2; i <= 775146; i++){
+  if(600851475143 % i === 0){
+    console.log(i)
   }
 }
-console.log(sum);
